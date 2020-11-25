991c7dbc6840a7b3a24dba0d1d6b22b491dcb30d
biccu <ssttsang014@gmail.com>
2020-11-25 18:00:20 +0900

    h2-console 실행 전

--- src/main/java/com/biccu/calculator/web/WebRestController.java
+++ src/main/java/com/biccu/calculator/web/WebRestController.java
@@ -1,13 +1,25 @@
 package com.biccu.calculator.web;
 
+import com.biccu.calculator.domain.PostsRepository;
+import com.biccu.calculator.dto.PostsSaveRequestDto;
+import lombok.AllArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@AllArgsConstructor
 public class WebRestController {
+
+    private PostsRepository postsRepository;
 
     @GetMapping("/hello")
     public String hello(){
         return "Hello World!";
+    }
+
+    @GetMapping("/posts")
+    public void savePosts(@RequestBody PostsSaveRequestDto dto){
+        postsRepository.save(dto.toEntity());
     }
 }
